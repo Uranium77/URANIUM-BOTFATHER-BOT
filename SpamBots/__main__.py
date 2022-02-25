55815fef96695d2451d77c8032ea0ff298802bed
THE_URANIUM <98459212+Uranium77@users.noreply.github.com>
2022-02-25 14:08:17 +0530

    Update __main__.py

--- SpamBots/__main__.py
+++ SpamBots/__main__.py
@@ -16,7 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Successfully deployed!")
-print("Enjoy! Do visit @savage_spam_bots")
+print("Enjoy! Do visit @RAJPUTANA_EMPIRE")
 
 if __name__ == "__main__":
     UstaD.run_until_disconnected()
